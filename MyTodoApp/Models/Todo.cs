8584d1a18aa3c55873247d5660bf9aa318e57f49
RbVs <r.b.vs@outlook.de>
2020-10-17 15:58:28 +0200

    Projektdateien hinzufügen.
    init clean project

---
+++ MyTodoApp/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Priority { get; set; }
+    }
+}
